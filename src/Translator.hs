527dca309f8c470090e387428afb5bd5f9580a76
drula <adudinov@yandex.by>
2018-11-04 17:02:23 +0300

    Initial dummy unit tests

--- src/Translator.hs
+++ src/Translator.hs
@@ -8,5 +8,5 @@ module Translator (
 -- | Translate Parsiuk code to C header code and C source code.
 -- The function is not yet implemented.
 translate :: String -> Either String (String, String)
-translate _ = Right ("Dummy C header code\n", "Dummy C source code\n")
--- translate _ = Left "Dummy error"
+-- translate _ = Right ("Dummy C header code\n", "Dummy C source code\n")
+translate _ = Left "The function is not yet implemented" -- FIXME: get the function name
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,13 @@
+import Test.Tasty
+import Test.Tasty.HUnit
+
+import Translator (translate)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+    defaultMain $ testGroup "Translator tests" [translateTest]
+
+translateTest :: TestTree
+translateTest = testCase "Testing translate" $
+    assertEqual "Checking if 'translate' is implemented" expectedResult (translate "blablabla")
+    where expectedResult = Left "The function is not yet implemented"
