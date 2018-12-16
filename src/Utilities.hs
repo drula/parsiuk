c4e3de84af473a4dbb8280f7e32b981847b79e8f
drula <adudinov@yandex.by>
2018-12-16 16:13:39 +0300

    Process Parsiuk file with .h and .c extensions
    If a Parsiuk file has .h or .c extension instead of .prl, replace
    .h or .c to _h and _c and add .h and .c extensions to generated
    C files.
    Add more unit tests for this utility.

--- src/Utilities.hs
+++ src/Utilities.hs
@@ -3,13 +3,18 @@ module Utilities (
     makeCFileNames
     ) where
 
-import System.FilePath (addExtension, dropExtension)
+import System.FilePath (addExtension, dropExtension, takeExtension, splitExtension)
 
 -- | Make C header and source file names from Parsiuk file name by changing
--- .prl extension to .h and .c
+-- .prl extension to .h and .c; if Parsiuk file has .h or .c extension, .h or .c are
+-- changed to _h or _c and then .h and .c extensions added
 makeCFileNames :: FilePath -> (FilePath, FilePath)
-makeCFileNames pSouceName = (cHeaderName, cSourceName)
+makeCFileNames pSouceName =
+    if extension == ".h" || extension == ".c"
+    then (makeMangledFileName "h", makeMangledFileName "c")
+    else (makeFileName "h", makeFileName "c")
     where
-        cHeaderName = addExtension baseFileName "h"
-        cSourceName = addExtension baseFileName "c"
-        baseFileName = dropExtension pSouceName
+        (baseFileName, extension) = splitExtension pSouceName
+        mangledFileName = baseFileName ++ "_" ++ tail extension
+        makeFileName = addExtension baseFileName
+        makeMangledFileName = addExtension mangledFileName
--- test/Spec.hs
+++ test/Spec.hs
@@ -6,14 +6,29 @@ import Utilities
 
 main :: IO ()
 main = do
-    defaultMain $ testGroup "Translator tests" [translateTest, makeCFileNamesTest]
+    defaultMain $ testGroup "Tests" [translateTest, makeCFileNamesTest]
 
 translateTest :: TestTree
-translateTest = testCase "Testing translate" $
-    assertEqual "Checking if 'translate' is implemented" (translate "blablabla") expectedResult
+translateTest = testCase "Translation" $
+    assertEqual "Checking if 'translate' is implemented" expectedResult $ translate "blablabla"
     where expectedResult = Left "The function 'translate' is not yet implemented"
 
 makeCFileNamesTest :: TestTree
-makeCFileNamesTest = testCase "Making C file names from Parsiuk file names" $
-    assertEqual "test.prl" (makeCFileNames "test.prl") ("test.h", "test.c")
-    -- TODO: add more tests
+makeCFileNamesTest = testGroup "Making C file names from Parsiuk file names"
+    [makeTest "test.prl" "test.h" "test.c",
+     makeTest "prl.prl" "prl.h" "prl.c",
+     makeTest "prl.abc" "prl.h" "prl.c",
+     makeTest "abc" "abc.h" "abc.c",
+     makeTest "prl" "prl.h" "prl.c",
+     makeTest "test.abc" "test.h" "test.c",
+     makeTest "test.h" "test_h.h" "test_h.c",
+     makeTest "test.c" "test_c.h" "test_c.c",
+     makeTest ".prl" ".h" ".c",
+     makeTest "abc." "abc.h" "abc.c",
+     makeTest "." ".h" ".c",
+     makeTest "" ".h" ".c"]
+    where
+        makeTest pSource cHeader cSource =
+            let testName = pSource ++ " => " ++ cHeader ++ ", " ++ cSource in
+            testCase testName $
+                assertEqual pSource (cHeader, cSource) $ makeCFileNames pSource
